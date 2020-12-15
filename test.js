25745f9ce56fd9adc561d966ba5feac358feeb84
GengHH <18818060415@163.com>
2020-12-15 11:34:34 +0800

    w：测试

---
+++ test.js
@@ -0,0 +1,20 @@
+(function() {
+	var tmp = 'hasdh';
+	function HashChangeEvent() {
+		console.log(tmp);
+	}
+	HashChangeEvent();
+})();
+
+/**
+ *
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns
+ */
+function haha(a, b) {
+	return a + b;
+}
+
+var arg = 2;
