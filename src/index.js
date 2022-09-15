cce6796bc8d78732268b4b379d53e9e9150fdd57
GengHH <18818060415@163.com>
2022-09-16 01:53:45 +0800

    🐛修复: 修复commit log 过多的问题

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
  * @Author: GengHH 18818060415@163.com
  * @Date: 2022-09-15 23:00:57
  * @LastEditors: GengHH 18818060415@163.com
- * @LastEditTime: 2022-09-16 01:39:31
+ * @LastEditTime: 2022-09-16 01:52:48
  * @FilePath: \gittest\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 let a = ''
 console.log('%c 🌰', 'color:#2eafb0', a + 'just test ...')
 let b
-let c = 2 * 11
+let c = 6 * 11
 console.log('%c 🍇 c', 'color:#465975', c)
